5eb0b73241367cfffa4403007a513bdce3835646
Tatiana <tanya_potapenko@bigmir.net>
2024-08-15 15:39:29 +0200

    make build

---
+++ index.813ed510.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".headline").forEach(function(e){e.addEventListener("click",function(t){var n=e.nextElementSibling;n&&"UL"===n.tagName&&"SPAN"===t.target.tagName&&("none"===getComputedStyle(n).display?n.style.display="block":n.style.display="none")})});
+//# sourceMappingURL=index.813ed510.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const headlines = document.querySelectorAll(".headline");
+headlines.forEach((headline)=>{
+    headline.addEventListener("click", (e)=>{
+        const list = headline.nextElementSibling;
+        if (list && list.tagName === "UL") {
+            if (e.target.tagName === "SPAN") {
+                const isListHidden = getComputedStyle(list).display === "none";
+                // getComputedStyle()-
+                // returns an object containing all the styles applied to the element;
+                if (isListHidden) list.style.display = "block";
+                else list.style.display = "none";
+            }
+        }
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
